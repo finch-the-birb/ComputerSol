5ab9ae80319e5256087a1382f771970fb2af7a2c
finch-the-birb <zyablikf03@gmail.com>
2022-10-12 20:57:26 +0300

    Added Server class

---
+++ src/main/java/org/example/Server.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class Server extends Computerlmpl {
+    public Server(String cpu,String ozu,String hdd) {
+        setCPU(cpu);
+        setHDD(ozu);
+        setOZU(hdd);
+        setModel("R2D2"); //ну, я не силён в фирмах серверов, потому вот пасхалка ;)
+    }
+}
