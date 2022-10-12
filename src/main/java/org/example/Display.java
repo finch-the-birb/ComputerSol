48d5be972e25ba89a02583c6c5a3f41b9b1c925e
finch-the-birb <zyablikf03@gmail.com>
2022-10-12 20:24:29 +0300

    Added Display class

---
+++ src/main/java/org/example/Display.java
@@ -0,0 +1,27 @@
+package org.example;
+
+public class Display {
+    private String connection;
+    private String resolution;
+
+    public String getResolution() {
+        return this.resolution;
+    }
+
+    public void setResolution (String resolution) {
+        this.resolution = resolution;
+    }
+
+    public String getConnectionDisplay() {
+        return connection;
+    }
+
+    public void setConnectionDisplay(String connection) {
+        this.connection = connection;
+    }
+
+    public Display() {
+        setConnectionDisplay("Display is connected to your device");
+    }
+}
+
