acea55e4d09b1336cb9d695e970db1f2ffd91e64
finch-the-birb <zyablikf03@gmail.com>
2022-10-12 20:13:45 +0300

    Added Desktop class

---
+++ src/main/java/org/example/Desktop.java
@@ -0,0 +1,74 @@
+package org.example;
+
+public class Desktop extends Computerlmpl{
+    private String Videocard;
+    private String Status = "Off";
+    private String InternetStatus = "Off";
+
+    public Desktop (String cpu,String ozu,String hdd,String videocard) {
+        setCPU(cpu);
+        setHDD(ozu);
+        setOZU(hdd);
+        setModel("Desktop");
+        this.Videocard = videocard;
+    }
+
+    public String getVideocard() {
+        return Videocard;
+    }
+
+    public void setVideocard(String videocard) {
+        this.Videocard = videocard;
+    }
+
+    @Override
+    public void turnOn() throws IllegalArgumentException {
+        switch (this.Status) {
+            case "Off" -> {
+                System.out.println(
+                        "Computer" + getModel() + "[Spec:" + getCPU() + "," + getHDD() + "," + getOZU()
+                                + "," + getVideocard() + "] turning On..."
+                );
+                this.Status = "On";
+            }
+            case "On" -> System.out.println("Already turned On");
+            default -> throw new IllegalArgumentException("Maybe this computer is broken...");
+        }
+    }
+
+    @Override
+    public void turnOff() throws IllegalArgumentException {
+        switch (this.Status) {
+            case "On" -> {
+                System.out.println(
+                        "Computer" + getModel() + "[Spec:" + getCPU() + "," + getHDD() + "," + getOZU()
+                                + "," + getVideocard() + "] turning Off..."
+                );
+                this.Status = "Off";
+            }
+            case "Off" -> System.out.println("Already turned Off");
+            default -> throw new IllegalArgumentException("Maybe this computer is broken...");
+        }
+    }
+
+    @Override
+    public void internetConnect() throws IllegalArgumentException {
+        switch (this.InternetStatus) {
+            case "Off" -> {
+                System.out.println(
+                        "Computer" + getModel() + "[Spec:" + getCPU() + "," + getHDD() + "," + getOZU()
+                                + "," + getVideocard() + "] connecting..."
+                );
+                this.InternetStatus = "On";
+            }
+            case "On" -> {
+                System.out.println(
+                        "Computer" + getModel() + "[Spec:" + getCPU() + "," + getHDD() + "," + getOZU()
+                                + "," + getVideocard() + "] disconnecting..."
+                );
+                this.InternetStatus = "Off";
+            }
+            default -> throw new IllegalArgumentException("Maybe this computer is broken...");
+        }
+    }
+}
