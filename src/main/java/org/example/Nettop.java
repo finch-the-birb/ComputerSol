a8426f518b4bd48dbabbca1ab3cbd47cdfbd52b6
finch-the-birb <zyablikf03@gmail.com>
2022-10-12 21:02:08 +0300

    Added Nettop class

---
+++ src/main/java/org/example/Nettop.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class Nettop extends Computerlmpl{
+    public Nettop (String cpu,String ozu,String hdd, String model) {
+        setCPU(cpu);
+        setHDD(ozu);
+        setOZU(hdd);
+        setModel(model);
+    }
+}
