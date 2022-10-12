5c05485526ceaa780d296f8a00f3286e366cab33
finch-the-birb <zyablikf03@gmail.com>
2022-10-12 22:56:22 +0300

    Added Netbook class

---
+++ src/main/java/org/example/Netbook.java
@@ -0,0 +1,55 @@
+package org.example;
+
+
+public class Netbook extends PortableComputer {
+    private String CPU;
+    private String OZU;
+    private String HDD;
+    private String Status = "Off";
+
+    public Netbook (String cpu,String ozu,String hdd, String model) {
+        setCPU(cpu);
+        setHDD(ozu);
+        setOZU(hdd);
+        setModel(model);
+    }
+
+    @Override
+    public void turnOn() throws IllegalArgumentException {
+        switch (this.Status) {
+            case "Off" -> {
+                System.out.println(
+                        "Computer" + getModel() + "[Spec:" + getCPU() + "," + getHDD() + "," + getOZU() +
+                                "] turning On..."
+                );
+                keyboard.setConnectionKeyboard("connected");
+                System.out.println("Keyboard" + keyboard.getConnectionKeyboard());
+                display.setConnectionDisplay("connected");
+                System.out.println("Display" + display.getConnectionDisplay());
+                this.Status = "On";
+            }
+            case "On" -> System.out.println("Already turned On");
+            default -> throw new IllegalArgumentException("Maybe this computer is broken...");
+        }
+    }
+
+
+    @Override
+    public void turnOff() throws IllegalArgumentException {
+        switch (this.Status) {
+            case "On" -> {
+                System.out.println(
+                        "Computer" + getModel() + "[Spec:" + getCPU() + "," + getHDD() + "," + getOZU() +
+                                "," + display.getResolution() + "] turning Off..."
+                );
+                keyboard.setConnectionKeyboard("disconnected");
+                System.out.println("Keyboard" + keyboard.getConnectionKeyboard());
+                display.setConnectionDisplay("disconnected");
+                System.out.println("Display" + display.getConnectionDisplay());
+                this.Status = "Off";
+            }
+            case "Off" -> System.out.println("Already turned Off");
+            default -> throw new IllegalArgumentException("Maybe this computer is broken...");
+        }
+    }
+}
